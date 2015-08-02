cde41b44a5b865b7eb9d3c9ab7ef7459b2e146eb
Albert Capdevila <albert.capdevila@acprogramador.com>
2015-08-02 23:54:41 +0200

    Carga inicial del proyecto

---
+++ GestionFacturas.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GestionFacturas.Website.MvcApplication" Language="C#" %>
---
+++ GestionFacturas.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionFacturas.Website.Startup))]
+namespace GestionFacturas.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
