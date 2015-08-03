5ac328ccb9cca21298aa53484f481098914615df
Albert Capdevila <albert@acprogramador.com>
2015-08-03 16:46:23 +0200

    Inicio definición de modelos

---
+++ GestionFacturas.Modelos/Factura.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionFacturas.Modelos
+{
+    public class Factura
+    {
+        public Factura()
+        {
+            Lineas = new List<LineaFactura>();
+        }
+        
+        public int Id { get; set; }
+
+        public int VendedorId { get; set; }
+        public int CompradorId { get; set; }
+
+
+        public string NumeroSerie { get; set; }
+        public int NumeroFactura { get; set; }
+        public string NumeroFormato { get; set; }
+       
+        public DateTime FechaEmisionFactura { get; set; }
+        public DateTime FechaVencimientoFactura { get; set; }
+
+        public int FormaPago { get; set; }
+        public bool EstaEnviada { get; set; }
+        public int  EstadoFactura { get; set; }
+        public string Comentarios { get; set; }
+        public string ComentariosPie { get; set; }
+        
+        public string RegistroCreadoPorUsuario { get; set; }
+        public DateTime RegistroCreadoEnFecha { get; set; }
+        public string RegistroEditadoPorUsuario { get; set; }
+        public DateTime RegistroEditadoEnFecha { get; set; }
+        
+        public virtual FormaJuridica Vendedor { get; set; }
+        public virtual FormaJuridica Comprador { get; set; }
+
+        public virtual ICollection<LineaFactura> Lineas { get; set; }
+        
+    }
+
+    public class LineaFactura
+    {
+
+    }
+}
---
+++ GestionFacturas.Modelos/FormaJuridica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionFacturas.Modelos
+{
+    public class FormaJuridica
+    {
+        public string NumeroIdentificacionFiscal { get; set; }
+        public string NombreOEmpresa { get; set; }    
+        public string Direccion { get; set; }
+        public string Localidad { get; set; }
+        public string Provincia { get; set; }
+        public string CodigoPostal { get; set; }
+        public string Pais { get; set; }
+    }
+
+    public class PersonaFisica : FormaJuridica
+    {
+        public string Apellidos { get; set; }
+        public string Nombre { get { return string.Format("{0} {1}", NombreOEmpresa, Apellidos).Trim(); } }
+    }
+
+    public class PersonaJuridica : FormaJuridica
+    {
+        public string NombreComercial { get; set; }
+        public string Nombre { get { return (NombreComercial ?? NombreOEmpresa).Trim(); } }
+    }
+
+}
