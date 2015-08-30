a82703f254a6eb173efede14faa12b69102515dd
Albert Capdevila <albert@acprogramador.com>
2015-08-30 19:15:27 +0200

    Plantilla de administración

--- GestionFacturas.Website/App_Start/BundleConfig.cs
+++ GestionFacturas.Website/App_Start/BundleConfig.cs
@@ -26,7 +26,8 @@ namespace GestionFacturas.Website
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/dashboard.css"));
         }
     }
 }
